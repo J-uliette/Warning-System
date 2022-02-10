03042d8c0ef7aa2cafa8ecbffa14298754e5baf1
foldfield <fao25@cam.ac.uk>
2022-02-10 16:50:54 +0000

    testing again

--- test_geo.py
+++ test_geo.py
@@ -29,7 +29,7 @@ def test_stations_by_distance():
     sorted_stations = geo.stations_by_distance(stations, p)
     assert len(stations) == len(sorted_stations)
     for i in range(len(stations) - 1):
-       assert sorted_stations[i][1] <= sorted_stations[i + 1][1]
+       assert sorted_stations[i][2] <= sorted_stations[i + 1][2]
 def test_rivers_by_station_number():
     #Build list of stations
     stations = build_station_list()
