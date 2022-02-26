715210059779cab1dc83cbf967a0e6871fdb1414
foldfield <fao25@cam.ac.uk>
2022-02-26 11:57:33 +0000

    2E

--- Task2E.py
+++ Task2E.py
@@ -1,5 +1,5 @@
 from matplotlib import pyplot as plt
-from datetime import datetime, timedelta, date
+import datetime
 from floodsystem.stationdata import build_station_list, update_water_levels
 from floodsystem.datafetcher import fetch_measure_levels
 from floodsystem.flood import stations_highest_rel_level
@@ -7,7 +7,7 @@ from floodsystem.plot import plot_water_levels
 stations = build_station_list()
 update_water_levels(stations)
 stationlist=stations_highest_rel_level(stations, 5)
-dt = 10
+dt = datetime.timedelta(10)
 
 
 for i in range(len(stationlist)):
