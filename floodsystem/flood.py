3c828a1aec0c583b7dd988361626ad36a2cf1c75
foldfield <fao25@cam.ac.uk>
2022-02-26 12:06:44 +0000

    comments

--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -8,12 +8,12 @@ def stations_level_over_threshold(stations, tol): #2B
     level at the station. It is sorted in descending 
     order of relative water levels'''
 
-    stations_over = []
+    stations_over = [] #empty list
 
     for station in stations: #iterating across all stations
-        if (type(station.latest_level) == float)  and (station.typical_range_consistent() == True):
-            if station.relative_water_level() > tol:
-                stations_over.append((station, station.relative_water_level()))
+        if (type(station.latest_level) == float)  and (station.typical_range_consistent() == True): #excludes NoneType and makes sure the range is consistent
+            if (station.relative_water_level() > tol) and (station.relative_water_level() < 50): #makes sure relative water level isn't ridiculously high (Letcombe Basset lol)
+                stations_over.append((station, station.relative_water_level())) #adds station to the list
     
 
     sorted_stations = sorted_by_key(stations_over, 1, True) #sorts by relative water level in descending order
@@ -27,9 +27,9 @@ def stations_highest_rel_level(stations, N):
     range, is highest. The list is sorted in 
     descending order by relative level.'''
             
-    stations_over = stations_level_over_threshold(stations, -1000)
+    stations_over = stations_level_over_threshold(stations, -1000) #tolerance is sufficiently (negatively) large to include every station
 
-    highest_stations = stations_over[:N]
+    highest_stations = stations_over[:N] #slices from [0] up to [N]
 
     sorted_stations = sorted_by_key(highest_stations, 1, True)
     #unnecessary as long as the stations_level_over_threshold works as it should
